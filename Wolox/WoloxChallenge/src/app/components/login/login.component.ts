57bbab408ff95960afe92bd43b1fd8bf0a92545e
Nicolas Cueto <ncueto@binit.com.ar>
2020-09-03 23:25:11 -0300

    login

---
+++ Wolox/WoloxChallenge/src/app/components/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../infrastructure/services/user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  email: string;
+  password: string;
+  rememberMe = false;
+
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    if(this.validateCredencials()){
+      this.userService.login(this.email, this.password, this.rememberMe);
+    }
+  }
+
+  private validateCredencials(){
+    let isValid = true;
+
+    if (!this.email || !this.email.includes('@')) {
+      // this.errors.email = 'USUARIO_VACIO';
+      isValid = false;
+    }
+
+    if (!this.password || this.password.length <= 6 ) {
+      // this.errors.password = 'CONTRASEÑA_VACIA';
+      isValid = false;
+    }
+
+    return isValid;
+  }
+
+}
