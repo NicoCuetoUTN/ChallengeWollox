30bc4d9d71bf5fabf6bcb48fb890ca85d48677af
Nicolas Cueto <ncueto@binit.com.ar>
2020-09-03 23:25:36 -0300

    technologies list

---
+++ Wolox/WoloxChallenge/src/app/components/technologies-list/technologies-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-technologies-list',
+  templateUrl: './technologies-list.component.html',
+  styleUrls: ['./technologies-list.component.css']
+})
+export class TechnologiesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
