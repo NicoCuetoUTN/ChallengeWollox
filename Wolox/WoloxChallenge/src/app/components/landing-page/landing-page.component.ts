6fb45caa210b117e4471c2baeb2b28bc32b5d7cf
Nicolas Cueto <ncueto@binit.com.ar>
2020-09-04 01:16:01 -0300

    redirect to technologies list

--- Wolox/WoloxChallenge/src/app/components/landing-page/landing-page.component.ts
+++ Wolox/WoloxChallenge/src/app/components/landing-page/landing-page.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../infrastructure/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styleUrls: ['./landing-page.component.css'],
 })
 export class LandingPageComponent implements OnInit {
+  constructor(public userService: UserService, private router: Router) {}
 
-  constructor(private userService:UserService) {}
+  ngOnInit() {
+    let userLogged = this.userService.isLoggedIn();
+    userLogged && this.router.navigate(['/TechnologiesList']);
+  }
 
-  ngOnInit(): void {
-
+  gotoExternalUrl(url: string) {
+    window.location.href = url;
   }
 }
